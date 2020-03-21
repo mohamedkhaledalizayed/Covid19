5f17343d0e10ff2a0c02eb6b6fa889c02d3e9f1c
khaled <mohamedsmile999@gmail.com>
2020-03-22 01:15:30 +0200

    mm

--- app/src/main/java/com/itgds/covid19/view/activity/SplashScreenActivity.kt
+++ app/src/main/java/com/itgds/covid19/view/activity/SplashScreenActivity.kt
@@ -1,7 +1,9 @@
 package com.itgds.covid19.view.activity
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
 import com.itgds.covid19.R
 
 class SplashScreenActivity : AppCompatActivity() {
@@ -9,5 +11,10 @@ class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed({
+            startActivity(Intent(this@SplashScreenActivity, MainActivity::class.java))
+            finish()
+        }, 5000)
     }
 }
