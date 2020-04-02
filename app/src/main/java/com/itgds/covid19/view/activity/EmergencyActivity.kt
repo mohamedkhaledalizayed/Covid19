3c1ad4ff4e28afc3b514d243a7cc966996a40b5d
khaled <mohamedsmile999@gmail.com>
2020-04-02 20:38:10 +0200

    done

--- app/src/main/java/com/itgds/covid19/view/activity/EmergencyActivity.kt
+++ app/src/main/java/com/itgds/covid19/view/activity/EmergencyActivity.kt
@@ -1,7 +1,10 @@
 package com.itgds.covid19.view.activity
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.itgds.covid19.R
 import kotlinx.android.synthetic.main.activity_information.*
@@ -11,6 +14,11 @@ class EmergencyActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_emergency)
-
         }
+
+    fun call(view: View) {
+        val call= Intent(Intent.ACTION_DIAL)
+        call.data = Uri.parse("tel:105")
+        startActivity(call)
+    }
 }
